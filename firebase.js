98846b7ba5293153a9af6be77b9c8d72fc61141f
Aliza Khan <alizaadi2@gmail.com>
2024-08-09 23:10:28 +0500

    Update firebase.js

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyADIRUGIYrAtNwt3HRrT4WU2pLRFaKxivk",
+  authDomain: "onifood-pk.firebaseapp.com",
+  projectId: "onifood-pk",
+  storageBucket: "onifood-pk.appspot.com",
+  messagingSenderId: "540168170457",
+  appId: "1:540168170457:web:ef12dcf1d487057a1687c8"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
