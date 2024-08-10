34d24809c254f327d03207a7fdbc5051600c306f
Aliza Khan <alizaadi2@gmail.com>
2024-08-10 23:55:18 +0500

    Update restaurants.js

--- Javascript files/restaurants.js
+++ Javascript files/restaurants.js
@@ -1,10 +1,21 @@
 const restaurantImage = document.getElementById("restaurant-image");
 const selectedImage = document.getElementById("selected-image");
+const addRestaurant = document.getElementById("add-restaurant");
+let file;
 
 restaurantImage.addEventListener("change", () => {
-   console.log(event.target.files[0]);
+    file = event.target.files[0];
+//    console.log(event.target.files[0]);
    selectedImage.style.display = "block";
    selectedImage.setAttribute("src", URL.createObjectURL(event.target.files[0]));
+})
+
+addRestaurant.addEventListener("click", () => {
+    const name = document.getElementById("restaurant-name");
+    const address = document.getElementById("restaurant-address");
+    const image = document.getElementById("restaurant-image");
+
+    console.log()
 })
 
 
