39bc6615de6728b1b170010732d942308524c0bb
Aliza Khan <alizaadi2@gmail.com>
2024-08-10 22:24:54 +0500

    Update app.js

--- app.js
+++ app.js
@@ -8,3 +8,25 @@ if (toastTrigger) {
   })
 }
 
+import {auth, signInWithEmailAndPassword} from "./firebase.js"
+
+const login = () => {
+  const email = document.getElementById("email");
+  const password = document.getElementById("password");
+
+  console.log(email.value, password.value)
+
+  signInWithEmailAndPassword(auth, email.value, password.value)
+  .then((userCredential) => {
+    const user = userCredential.user;
+    console.log(user);
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    console.log(errorMessage);
+  });
+}
+ 
+const loginBtn = document.getElementById("loginBtn");
+loginBtn && loginBtn.addEventListener('click', login);
