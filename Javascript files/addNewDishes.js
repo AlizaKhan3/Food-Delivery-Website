ae73e4f9e5a20311fbb9e589ad38c1d3c0042fa0
Aliza Khan <alizaadi2@gmail.com>
2024-08-18 01:02:44 +0500

    Update addNewDishes.js

--- Javascript files/addNewDishes.js
+++ Javascript files/addNewDishes.js
@@ -1,4 +1,5 @@
 import { storage, ref, uploadBytesResumable, getDownloadURL, db, collection, addDoc, getDocs } from "./firebase.js"
+import { uploadFile } from "./restaurants.js"
 
 const getAllRestaurants = async () => {
     const restSelect = document.getElementById("restaurant-name");
@@ -17,23 +18,35 @@ const getAllRestaurants = async () => {
 
 getAllRestaurants();
 
-const addDish = document.getElementById("add-dish");
-addDish.addEventListener('click', () => {
+const addDish = document.getElementById("addDish");
+addDish.addEventListener('click', async () => {
     const restName = document.getElementById("restaurant-name");
     const dishName = document.getElementById("dish-name");
     const dishPrice = document.getElementById("dish-price");
     const dishServing = document.getElementById("dish-serving");
     const dishImage = document.getElementById("dish-image");
+    const image = await uploadFile(dishImage, dishName.value);
+    const spinner = document.getElementById("dish-spinner");
+    const closebtn = document.getElementById("close-btn");
 
+    spinner.style.display = "block";
     const dishDetail = {
         restaurant: restName.value,
         name: dishName.value,
         price: dishPrice.value,
         serving: dishServing.value,
-        image: dishImage.value
+        image
     }
+    const docRef = await addDoc(collection(db, "dishes"), dishDetail);
+    restName.value = "";
+    dishName.value = "";
+    dishPrice.value = "";
+    dishServing.value = "";
+    dishImage.value = "";
+    spinner.style.display = "none";
+    closebtn.click();
 
-    console.log(dishDetail);
+    console.log(docRef);
 })
 
 
