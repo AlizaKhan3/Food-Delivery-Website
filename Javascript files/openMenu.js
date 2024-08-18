3786bb3831d38017a888064d741882c238f79b23
Aliza Khan <alizaadi2@gmail.com>
2024-08-18 21:16:26 +0500

    Update openMenu.js

--- Javascript files/openMenu.js
+++ Javascript files/openMenu.js
@@ -1,4 +1,4 @@
-import { collection, db, collection, getDocs, where, query } from "./firebase.js"
+import { db, collection, getDocs, where, query } from "./firebase.js"
 
 const getAllDishes = async () => {
     const allDishes = document.getElementById("all-dishes");
@@ -7,55 +7,31 @@ const getAllDishes = async () => {
     allDishes.innerHTML = ``
     querySnapshot.forEach((doc) => {
         allDishes.innerHTML += `
-             <div class="card mb-3 p-2 w-100">
-                    <div class="row g-0">
-                        <div class="col-md-4">
-                            <img src="https://images.deliveryhero.io/image/fd-pk/LH/n1zj-listing.jpg?width=400&height=225"
-                                class="img-fluid rounded-start w-100" style="border-radius: 1rem;" alt="...">
-                            <div class="card-img-overlay">
-                                <button type="button" class="btn btn-primary" id="liveToastBtn">Avail 25% Off</button>
-                                <div class="toast-container position-fixed bottom-0 end-0 p-3">
-                                    <div id="liveToast" class="toast" role="alert" aria-live="assertive"
-                                        aria-atomic="true">
-                                        <div class="toast-header">
-                                            <!-- <img src="..." class="rounded me-2" alt="..."> -->
-                                            <strong class="me-auto">OniFood</strong>
-                                            <small>11 mins ago</small>
-                                            <button type="button" class="btn-close" data-bs-dismiss="toast"
-                                                aria-label="Close"></button>
-                                        </div>
-                                        <div class="toast-body">
-                                            Hello, world! This is a toast message.
-                                        </div>
-                                    </div>
+         <div class="card dish-card mb-3 w-100">
+                        <div class="row g-0">
+                            <div class="col-2">
+                                <img src="images/Crispy chicken burger.webp" class="img-fluid rounded-start dish-image"
+                                    alt="...">
+                            </div>
+                            <div class="col">
+                                <div class="card-body">
+                                    <h5 class="card-title" id="menu-title" style="font-weight: bolder;">${doc.data().name}</h5>                          
+                                    <p class="card-text my-2" id="menu-text" style="color: #787878;"> crispy chicken
+                                        piece, sandwiched between full layered cheese, tomatoes, lettuce, onions and
+                                        mushroom sauce.</p>
+                                    <p class="card-text my-1" id="menu-text" style="color: #e44e3f;"> 799 Rs</p>
+                                    </p>
                                 </div>
                             </div>
-                        </div>
-                        <div class="col-md-8">
-                            <div class="card-body"> 
-                              <div class="d-flex justify-content-between">
-                                  <div>
-                                    <p class="card-text">
-                                        <span class="badge text-bg-primary px-4 py-2">Burgers</span>
-                                        <span class="badge text-bg-primary px-4 py-2">BBQ</span>
-                                        <span class="badge text-bg-primary px-4 py-2">Roti</span>
-                                        <span class="badge text-bg-primary px-4 py-2">Drinks</span>
-                                    </p>
-                                    <h5 class="card-title" style="font-weight: bolder;">Yum Foods</h5>
-                                    <p class="card-text"><small class="text-body-secondary">Last updated 3 mins
-                                            ago</small>
-                                    </p>
-                                </div>
-                                <div class="d-flex align-items-baseline">
-                                    <div>
-                                        <i class="fa-solid fa-phone social-icons"> <span>+92 3214567890</span> </i>
-                                    </div>
-                                </div>
-                              </div>
+                            <div class="col d-flex justify-content-end align-items-center mx-2 gap-2">
+                                <button class="qty-btn"><i class="fa-solid fa-minus"></i></button>
+                                <span class="fw-bold"> 1 </span>
+                                <button class="qty-btn"><i class="fa-solid fa-plus"></i></button>
+                                <button class="btn btn-primary cart-btn" type="submit">Add to Cart</button>
                             </div>
                         </div>
                     </div>
-                </div>
+             
         `
     })
 }
