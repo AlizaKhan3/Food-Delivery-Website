4c498a2f035046432dc76853b0e27c097279d42d
Aliza Khan <alizaadi2@gmail.com>
2024-08-20 16:52:23 +0500

    Update openMenu.js

--- Javascript files/openMenu.js
+++ Javascript files/openMenu.js
@@ -4,7 +4,6 @@ var urlParams = new URLSearchParams(window.location.search);
 let pageSpinner = document.getElementById("page-spinner");
 let mainContent = document.getElementById("main-content");
 
-
 const getRestaurantDetails = async () => {
     const resToast = document.getElementById("res-toast");
     const resName = document.getElementById("res-name");
@@ -20,16 +19,16 @@ const getRestaurantDetails = async () => {
         resAddress.innerHTML = docSnap.data().address;
         resImage.src = docSnap.data().image;
 
-        console.log("Document data", docSnap.data());    
+        console.log("Document data", docSnap.data());
     } else {
-        console.log("No Document data");    
+        console.log("No Document data");
     }
 }
 getRestaurantDetails();
 
 const getAllDishes = async () => {
     const allDishes = document.getElementById("all-dishes");
-    const q = query(collection(db, "dishes"), where ('restaurant' , "==", urlParams.get('restaurant')));
+    const q = query(collection(db, "dishes"), where('restaurant', "==", urlParams.get('restaurant')));
     console.log(urlParams.get('restaurant'))
 
     const querySnapshot = await getDocs(q);
@@ -53,53 +52,22 @@ const getAllDishes = async () => {
                                     </p>
                     </div>
                 </div>
-                <div class="d-flex align-items-center gap-2">
-                    <button onclick="updateQty('-','${
-                      doc.id
-                    }')" class="qty-btn"><i class="fa-solid fa-minus"></i></button>
-                    <span class="fw-bold" id="${doc.id}">1</span>
-                    <button onclick="updateQty('+','${
-                      doc.id
-                    }')" class="qty-btn"><i class="fa-solid fa-plus"></i></button>
-
-                    <a href="#" class="btn btn-primary" onclick="addToCart('${
-                      doc.id
-                    }')">Add to cart</a>
-                </div>
+                <div class="col d-flex justify-content-end align-items-center mx-2 gap-2">
+                    <button onclick="updateQty('-')" class="qty-btn"><i class="fa-solid fa-minus"></i></button>
+                    <span class="fw-bold"> 1 </span>
+                    <button onclick="updateQty('+')" class="qty-btn"><i class="fa-solid fa-plus"></i></button>
+                    <a href="#" class="btn btn-primary" onclick="addToCart('${doc.id}')">Add to cart</a>                            </div>
+                </div> 
             </div>
-        </div>
-    </div>
-        `;
-  });
+        </div>`;
+    });
 };
-
 
 getAllDishes();
 
+const updateQty = (type) => {
+    console.log(event.target)
+   console.log(type);
+}
 
-// allDishes.innerHTML += `
-//          <div class="card dish-card mb-3 w-100"> 
-//                         <div class="row g-0">
-//                             <div class="col-2">
-//                                 <img id="menu-image" src="${doc.data().image}"
-//                                     alt="...">
-//                             </div>
-//                             <div class="col">
-//                                 <div class="card-body">
-//                                     <h5 class="card-title" id="menu-title" style="font-weight: bolder;">${doc.data().name}
-//                                     </h5>
-//                                     <p class="card-text my-2" id="menu-text" style="color: #787878;">${doc.data().description}</p>
-//                                     <p class="card-text my-2" id="menu-text" style="color: #787878;">${doc.data().serving}</p>
-//                                     <p class="card-text my-1" id="menu-text" style="color: #e44e3f;">${doc.data().price}</p>
-//                                     </p>
-//                                 </div>
-//                             </div>
-//                             <div class="col d-flex justify-content-end align-items-center mx-2 gap-2">
-//                                 <button class="qty-btn"><i class="fa-solid fa-minus"></i></button>
-//                                 <span class="fw-bold"> 1 </span>
-//                                 <button class="qty-btn"><i class="fa-solid fa-plus"></i></button>
-//                                 <button class="btn btn-primary cart-btn" type="submit">Add to Cart</button>
-//                             </div>
-//                         </div>
-//                     </div>
-//         `
+window.updateQty = updateQty;
