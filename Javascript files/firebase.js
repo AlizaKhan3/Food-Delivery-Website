c0f35206932d35e8fd065462ff8f71afde9ad2a2
Aliza Khan <alizaadi2@gmail.com>
2024-08-23 23:25:16 +0500

    Update firebase.js

--- Javascript files/firebase.js
+++ Javascript files/firebase.js
@@ -3,15 +3,7 @@ import { getAuth, signInWithEmailAndPassword, onAuthStateChanged} from "https://
 import { getStorage, ref, uploadBytesResumable, getDownloadURL } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-storage.js";
 import { doc, setDoc, getFirestore, addDoc, getDocs, getDoc, collection, where, query, serverTimestamp} from "https://www.gstatic.com/firebasejs/10.12.5/firebase-firestore.js";
 // createUserWithEmailAndPassword, signOut, sendEmailVerification, googleProvider, facebookProvider, facebookAuthProvider, RecaptchaVerifier, SignInWithPhoneNumber, GoogleAuthProvider,signInWithPopup 
-      
-const firebaseConfig = {
-  apiKey: "AIzaSyDzpnkFF0DiZ32MVAUaMiTEu83A7QAUnx8",
-  authDomain: "onlinefood-7799a.firebaseapp.com",
-  projectId: "onlinefood-7799a",
-  storageBucket: "onlinefood-7799a.appspot.com",
-  messagingSenderId: "119235898205",
-  appId: "1:119235898205:web:9a6ca0302608a92a7556b4"
-};
+
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
