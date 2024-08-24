6cbd911cb355f788b6384c22ba721bd5a6869472
Aliza Khan <alizaadi2@gmail.com>
2024-08-24 18:31:13 +0500

    Update app.js

--- app.js
+++ app.js
@@ -35,36 +35,68 @@ const login = () => {
 const loginBtn = document.getElementById("loginBtn");
 loginBtn && loginBtn.addEventListener('click', login);
 
-
-
 // Show restaurants on home page
 const getAllRestaurants = async () => {
-  const pageSpinner = document.getElementById("page-spinner")
+  const pageSpinner = document.getElementById("page-spinner");
   const resList = document.getElementById("res-list");
-  resList.innerHTML = "";
-  const q = collection(db, "restaurants");
-  const querySnapshot = await getDocs(q);
-  let index = 0;
-  pageSpinner.style.display = "none";
-  querySnapshot.forEach((doc) => {
-    console.log(doc.id, " => ", doc.data());
-    resList.innerHTML += `
+
+  if (pageSpinner && resList) {
+    pageSpinner.style.display = "block";
+    resList.innerHTML = "";
+
+    const q = collection(db, "restaurants");
+    const querySnapshot = await getDocs(q);
+
+    pageSpinner.style.display = "none";
+
+    querySnapshot.forEach((doc) => {
+      console.log(doc.id, " => ", doc.data());
+      resList.innerHTML += `
         <div class="col">
-        <div class="card" h-100">
+          <div class="card" h-100">
             <img src="${doc.data().image}"
                 class="card-img-top" alt="..." loading="lazy">
             <div class="card-body">
               <a href="dishes.html?restaurant=${doc.id}"><h5 class="card-title" id="rest-display-card">${doc.data().name}</h5></a>
               <p class="card-text">
-              <span class="badge badge-color text-bg-primary">Fastfood</span>
-              <span class="badge badge-color text-bg-primary">Drinks</span>
+                <span class="badge badge-color text-bg-primary">Fastfood</span>
+                <span class="badge badge-color text-bg-primary">Drinks</span>
               </p>
             </div>
+          </div>
         </div>
-        </div>
-        `;
-  });
+      `;
+    });
+  }
 };
+// const getAllRestaurants = async () => {
+//   const pageSpinner = document.getElementById("page-spinner")
+//   const resList = document.getElementById("res-list");
+//   resList.innerHTML = "";
+//   const q = collection(db, "restaurants");
+//   const querySnapshot = await getDocs(q);
+//   let index = 0;
+//   pageSpinner.style.display = "none";
+//   querySnapshot.forEach((doc) => {
+//     console.log(doc.id, " => ", doc.data());
+//     resList.innerHTML += `
+//         <div class="col">
+//         <div class="card" h-100">
+//             <img src="${doc.data().image}"
+//                 class="card-img-top" alt="..." loading="lazy">
+//             <div class="card-body">
+//               <a href="dishes.html?restaurant=${doc.id}"><h5 class="card-title" id="rest-display-card">${doc.data().name}</h5></a>
+//               <p class="card-text">
+//               <span class="badge badge-color text-bg-primary">Fastfood</span>
+//               <span class="badge badge-color text-bg-primary">Drinks</span>
+//               </p>
+//             </div>
+//         </div>
+//         </div>
+//         `;
+//   });
+// };
+
 onAuthStateChanged(auth,(user) => {
   if (user && location.pathname.indexOf("restaurants") !== -1) {
     getAllRestaurants();  
