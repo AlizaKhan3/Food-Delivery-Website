45bb60637f4940b79c8eb8d2c5b8879bffa0c365
Aliza Khan <alizaadi2@gmail.com>
2024-08-24 23:40:05 +0500

    Update app.js

--- app.js
+++ app.js
@@ -50,7 +50,7 @@ const getAllRestaurants = async () => {
     pageSpinner.style.display = "none";
 
     querySnapshot.forEach((doc) => {
-      console.log(doc.id, " => ", doc.data());
+      // console.log(doc.id, " => ", doc.data());
       resList.innerHTML += `
         <div class="col">
           <div class="card" h-100">
@@ -69,40 +69,12 @@ const getAllRestaurants = async () => {
     });
   }
 };
-// const getAllRestaurants = async () => {
-//   const pageSpinner = document.getElementById("page-spinner")
-//   const resList = document.getElementById("res-list");
-//   resList.innerHTML = "";
-//   const q = collection(db, "restaurants");
-//   const querySnapshot = await getDocs(q);
-//   let index = 0;
-//   pageSpinner.style.display = "none";
-//   querySnapshot.forEach((doc) => {
-//     console.log(doc.id, " => ", doc.data());
-//     resList.innerHTML += `
-//         <div class="col">
-//         <div class="card" h-100">
-//             <img src="${doc.data().image}"
-//                 class="card-img-top" alt="..." loading="lazy">
-//             <div class="card-body">
-//               <a href="dishes.html?restaurant=${doc.id}"><h5 class="card-title" id="rest-display-card">${doc.data().name}</h5></a>
-//               <p class="card-text">
-//               <span class="badge badge-color text-bg-primary">Fastfood</span>
-//               <span class="badge badge-color text-bg-primary">Drinks</span>
-//               </p>
-//             </div>
-//         </div>
-//         </div>
-//         `;
-//   });
-// };
 
 onAuthStateChanged(auth,(user) => {
-  if (user && location.pathname.indexOf("restaurants") !== -1) {
+  if (user && location.pathname.indexOf("restaurants") !== -1 || location.pathname === "/") {
     getAllRestaurants();  
     console.log("hogaya ab sahi load")
   }
-
   // getAllRestaurants();
 })
 getAllRestaurants();  
