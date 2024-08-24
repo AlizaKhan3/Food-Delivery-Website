7b281bd11a463652f21ce184afaa9996ab3757e3
Aliza Khan <alizaadi2@gmail.com>
2024-08-24 16:46:47 +0500

    Update order.js
    updated status for orders

--- Javascript files/order.js
+++ Javascript files/order.js
@@ -5,15 +5,24 @@ placeOrder && placeOrder.addEventListener('click', async () => {
     const customerName = document.getElementById("customer-name");
     const customerPhone = document.getElementById("customer-phone");
     const customerAddress = document.getElementById("customer-address");
+    // const totalSum = document.getElementById("total-amount-subT")
     const cartItemsRemove = document.getElementById("cart");
     const modalBtn = document.getElementById("modalBtn")
     const cart = JSON.parse(localStorage.getItem("cart"));
+
+    const totalSubT = document.getElementById("total-amount-subT");
+    const totatCartSum = document.getElementById("total-amount-menu");
+    const sum = cart.reduce((a, b) => a + Number(b.price) * b.qty, 0);
+    totatCartSum.innerHTML = `${sum} Rs`;
+    totalSubT.innerHTML = `${sum - 150 + 10 + 99} Rs`;
+
     console.log(cart);
     console.log(customerName.value, customerAddress.value, customerPhone.value);
     const orderDetails = {
         customerName: customerName.value,
-        customerAddress: customerAddress.value,
         customerPhone: customerPhone.value,
+        customerAddress: customerAddress.value,
+        totalSum : sum,
         status: "pending",
         cart,
         timestamp: serverTimestamp()
@@ -41,20 +50,37 @@ placeOrder && placeOrder.addEventListener('click', async () => {
 const getAllOrders = async () => {
     const allOrders = document.getElementById("all-orders")
     const pageSpinner = document.getElementById("page-spinner");
+    const mainContent = document.getElementById("main-content-order")
     const q = collection(db, "orders");
     const querySnapshot = await getDocs(q);
+    let index =  0;
     querySnapshot.forEach(doc => {
+        index++;
         console.log("orders", doc.data());
-        allOrders.innerHTML = `
-        <th scope="row">${index}</th>
-            <td><img src="${doc.data().image}" class="dish-image" alt=""></td>
-            <td>${doc.data().name}</td>
-            <td>${doc.data().description}</td>
-            <td>${doc.data().price}</td>
-            <td>${doc.data().serving}</td>
-            <td>${restaurantNames[doc.data().restaurant]}</td>`
+
+        let status = doc.data().status;
+        let statusColor = "";
+
+        if (status === "pending") {
+            statusColor = "text-bg-warning";
+        }
+        if (status === "delivered") {
+            statusColor = "text-bg-success";
+        }
+        if (status === "confirmed") {
+            statusColor = "text-bg-info";
+        }
+        allOrders.innerHTML += `
+            <td>${index}</td>
+            <td>${doc.data().customerName}</td>
+            <td>${doc.data().customerPhone}</td>
+            <td>${doc.data().customerAddress}</td>
+            <td>${doc.data().totalSum}</td>
+            <td><span class="badge ${statusColor}">${status}</span></td>
+            `
     });
     pageSpinner.style.display = "none";
+    mainContent.style.display = "block";
 }
 
 getAllOrders();
