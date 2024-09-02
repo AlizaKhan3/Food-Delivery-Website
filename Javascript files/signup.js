419b542280189bdfff2e1aa74a7ce79c250348d1
Aliza Khan <alizaadi2@gmail.com>
2024-09-03 02:34:38 +0500

    Update signup.js

--- Javascript files/signup.js
+++ Javascript files/signup.js
@@ -1,61 +1,33 @@
-// import {auth, createUserWithEmailAndPassword,db, RecaptchaVerifier, signInWithEmailAndPassword, googleProvider, signInWithPopup, GoogleAuthProvider, facebookAuthProvider, facebookProvider, SignInWithPhoneNumber, doc, setDoc} from "./firebase.js";
+import {auth, createUserWithEmailAndPassword} from "../Javascript files/firebase.js"
 
-// let confirmation;
+const register = () => {
+    // const firstName = document.getElementById("firstName");
+    // const lastName = document.getElementById("lastName");
+    const email = document.getElementById("email");
+    const password = document.getElementById("password");
+    console.log("User Registered");
+    console.log(email.value)
 
-// const register = ( ) => {
-//     const phone = document.getElementById("phone");
-//     window.RecaptchaVerifier = new RecaptchaVerifier(auth, 'recaptcha-container', {});
-//     const appVerifier = window.recaptchaVerifier;
-//     console.log("phone-->", phone.value);
-//     SignInWithPhoneNumber(auth, `+${phone.value}`, appVerifier)
-//     .then((confirmationResult) => {
-//         console.log("sms sent to the user");
-//          confirmation = confirmationResult;
-//     }).catch((error) => {
-//         console.log("error message: ", error)
-//     });    
-// }
+    createUserWithEmailAndPassword(auth, email.value, password.value)
+    .then((userCredential) => {
+      // Signed up 
+      const user = userCredential.user;
+      console.log("user-->", user)
+      console.log("hogaya")
+    //   window.location.href = "../login.html";
+    },3000)
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      // console.log("errorMessage", errorMessage)
+    //   Swal.fire({
+    //     icon: "error",
+    //     title: "Oops...",
+    //     text: "Something went wrong! " +  errorMessage,
+    //   });
+    });
+}
 
+let registerBtn = document.getElementById("registerBtn");
+registerBtn.addEventListener("click", register);
 
-// let signInWithGoogle = () => {
-// signInWithPopup(auth, googleProvider)
-//  .then((result) => {
-//     // This gives you a Google Access Token. You can use it to access the Google API.
-//     const credential = GoogleAuthProvider.credentialFromResult(result);
-//     const token = credential.accessToken;
-//     // The signed-in user info.
-//     const user = result.user;
-//     // IdP data available using getAdditionalUserInfo(result)
-//     // ...
-//   }).catch((error) => {
-//     // Handle Errors here.
-//     const errorCode = error.code;
-//     const errorMessage = error.message;
-//     // The email of the user's account used.
-//     const email = error.customData.email;
-//     // The AuthCredential type that was used.
-//     const credential = GoogleAuthProvider.credentialFromError(error);
-//     // ...
-//   });
-// }
- 
-
-
-  
-// // const auth = getAuth();
-// signOut(auth).then(() => {
-//   // Sign-out successful.
-// }).catch((error) => {
-//   // An error happened.
-// });
-
-
-
-// let addUserToFirestor = async () => {
-//     const data = await setDoc(doc(db, "users", user.uid), {
-//         name: "Los Angeles",
-//         state: "CA",
-//         country: "USA"
-//     })
-
-// }
